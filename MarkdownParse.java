c4ae58fdaef83b0ecc7ec4517b7f4d76244c04a3
zayverrulez <103228598+zayverrulez@users.noreply.github.com>
2022-04-14 19:52:56 -0700

    Lab 3 Part 5.2

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -3,7 +3,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList; 
+import java.util.ArrayList;
 
 public class MarkdownParse {
 
@@ -11,27 +11,30 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            if (openBracket != 0 && openBracket - 1 == markdown.indexOf("!", currentIndex)) {
-                currentIndex = closeParen + 1;
+        while (currentIndex < markdown.length()) {
+            if (markdown.indexOf("[", currentIndex) != -1 && markdown.indexOf(")", markdown.indexOf("[", currentIndex)) != -1) {
+                int openBracket = markdown.indexOf("[", currentIndex);
+                int closeBracket = markdown.indexOf("]", openBracket);
+                int openParen = markdown.indexOf("(", closeBracket);
+                int closeParen = markdown.indexOf(")", openParen);
+                if (openBracket != 0 && openBracket - 1 == markdown.indexOf("!", currentIndex)) {
+                    currentIndex = closeParen + 1;
+                } else {
+                    toReturn.add(markdown.substring(openParen + 1, closeParen));
+                    currentIndex = closeParen + 1;
+                }
             } else {
-                toReturn.add(markdown.substring(openParen + 1, closeParen));
-                currentIndex = closeParen + 1;
+                break;
             }
         }
 
         return toReturn;
     }
 
-
     public static void main(String[] args) throws IOException {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
     }
 }
---
+++ test-file4.md
@@ -0,0 +1,7 @@
+[Link2](https://vimeo.com)
+[Link1](https://youtube.com)
+
+I am writing I am talking more (btw this 
+and that.)
+
+[Test]
